21b418b7f46e0f81bdc2f1090d442ee8db46e6e5
José Ignacio Amelivia Santiago <hello@namelivia.com>
2025-04-22 13:09:16 +0200

    Use api tokens

--- kimai/config/config.py
+++ kimai/config/config.py
@@ -9,20 +9,15 @@ def _get_config_from_yaml():
     with open(yaml_file_location, "r") as config_file:
         data = yaml.safe_load(config_file)
         
-        user = data.get("user")
         token = data.get("token")
         url = data.get("url")
         
-        return user, token, url
-
-def get_user():
-    user,_,_ = _get_config_from_yaml()
-    return user
+        return token, url
 
 def get_token():
-    _,token,_ = _get_config_from_yaml()
+    token,_ = _get_config_from_yaml()
     return token
 
 def get_url():
-    _,_,url = _get_config_from_yaml()
+    _,url = _get_config_from_yaml()
     return url + "/api/"
